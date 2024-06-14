4ad844c8cd21853613a97f4b9ba886b59a5e2985
Aitor <acalvo049@ehu.eus>
2024-06-14 16:32:57 +0200

    add i3

---
+++ i3/.config/i3/doublescreen.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+LAPTOP=`xrandr | awk '{if ($2=="connected" && $1 ~ /eDP/) print $1}'`
+MONITOR=`xrandr | awk '{if ($2=="connected" && $1 ~ /HDMI/) print $1}'`   
+
+xrandr --output $LAPTOP --auto
+xrandr --output $MONITOR --auto --left-of $LAPTOP
