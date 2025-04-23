07fba696631830a54c6ffa8ff36df0dab0bbf773
Aitor <acalvo049@ehu.eus>
2025-04-23 07:35:23 +0200

    disable update notifications on startup

--- nvim/.config/nvim/lua/lenovo/lazy.lua
+++ nvim/.config/nvim/lua/lenovo/lazy.lua
@@ -29,5 +29,5 @@ require("lazy").setup({
     install = { colorscheme = { "habamax" } },
 
     -- automatically check for plugin updates
-    checker = { enabled = true },
+    checker = { enabled = true , notify = false },
 })
