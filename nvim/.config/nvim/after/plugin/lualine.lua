5679e15e1f3c9f00039b222eb2fb4badc6d01696
Aitor <acalvo049@ehu.eus>
2025-04-23 07:35:11 +0200

    add lazy updates field

--- nvim/.config/nvim/after/plugin/lualine.lua
+++ nvim/.config/nvim/after/plugin/lualine.lua
@@ -26,7 +26,14 @@ lualine.setup {
   sections = {
     lualine_a = {'mode'},
     lualine_b = {'branch', 'diff', 'diagnostics'},
-    lualine_c = {'filename'},
+    -- lualine_c = {'filename'},
+    lualine_c = {
+        {
+            require("lazy.status").updates,
+            cond = require("lazy.status").has_updates,
+            color = { fg = "#ff9e64" },
+        },
+    },
     --lualine_x = {'encoding', 'fileformat', 'filetype'},
     lualine_x = {'fileformat', 'filetype'},
     lualine_y = {'progress'},
