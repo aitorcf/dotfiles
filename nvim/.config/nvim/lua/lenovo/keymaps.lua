68e1bf16504f9e910092b767c769ed46f6727777
Aitor Calvo Fernández <aitor.calvof@ehu.eus>
2025-12-09 12:38:59 +0100

    add config for molten notebook in nvim (unused)

--- nvim/.config/nvim/lua/lenovo/keymaps.lua
+++ nvim/.config/nvim/lua/lenovo/keymaps.lua
@@ -94,3 +94,14 @@ map( "n" , "<Tab>h"      , ":tabprevious<CR>" )
 map( "n" , "<Tab>o"      , ":+tabmove<CR>" )
 map( "n" , "<Tab>y"      , ":-tabmove<CR>" )
 
+-- -- Molten keymap
+-- vim.keymap.set("n", "<localleader>ip", function()
+--   local venv = os.getenv("VIRTUAL_ENV") or os.getenv("CONDA_PREFIX")
+--   if venv ~= nil then
+--     -- in the form of /home/benlubas/.virtualenvs/VENV_NAME
+--     venv = string.match(venv, "/.+/(.+)")
+--     vim.cmd(("MoltenInit %s"):format(venv))
+--   else
+--     vim.cmd("MoltenInit python3")
+--   end
+-- end, { desc = "Initialize Molten for python3", silent = true })
