54ade401a5048a76008a81cfa05a637372d39d49
Aitor Calvo Fernández <aitor.calvof@ehu.eus>
2025-12-09 12:38:26 +0100

    set textwidth for julia files

--- nvim/.config/nvim/lua/lenovo/autocommands.lua
+++ nvim/.config/nvim/lua/lenovo/autocommands.lua
@@ -2,4 +2,5 @@ vim.cmd[[
     au BufRead,BufNewFile *.tex setlocal textwidth=60
     au BufRead,BufNewFile *.txt setlocal textwidth=60
     au BufRead,BufNewFile *.md setlocal textwidth=60
+    au BufRead,BufNewFile *.jl setlocal textwidth=80
 ]]
