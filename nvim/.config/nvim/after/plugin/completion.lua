bac8c349eac40cc19d545f081eea7c9ea2185bda
Aitor Calvo Fernández <aitor.calvof@ehu.eus>
2025-12-09 12:37:54 +0100

    remove old lsp config

--- nvim/.config/nvim/after/plugin/completion.lua
+++ nvim/.config/nvim/after/plugin/completion.lua
@@ -129,19 +129,3 @@ cmp.setup.cmdline(':', {
 
 -- Set up lspconfig.
 local capabilities = require('cmp_nvim_lsp').default_capabilities()
--- Replace <YOUR_LSP_SERVER> with each lsp server you've enabled.
-require('lspconfig')['lua_ls'].setup {
-  capabilities = capabilities
-}
-require('lspconfig')['clangd'].setup {
-  capabilities = capabilities
-}
-require('lspconfig')['pyright'].setup {
-  capabilities = capabilities
-}
-require('lspconfig')['vimls'].setup {
-  capabilities = capabilities
-}
-require('lspconfig')['julials'].setup {
-  capabilities = capabilities
-}
