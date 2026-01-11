8003a80ab71bd46f8249b7e3f368a480a885c369
Aitor Calvo Fernández <aitor.calvof@ehu.eus>
2026-01-12 00:00:24 +0100

    add clojure linewidth

--- nvim/.config/nvim/lua/lenovo/autocommands.lua
+++ nvim/.config/nvim/lua/lenovo/autocommands.lua
@@ -4,4 +4,5 @@ vim.cmd[[
     au BufRead,BufNewFile *.md setlocal textwidth=60
     au BufRead,BufNewFile *.jl setlocal textwidth=80
     au BufRead,BufNewFile *.py setlocal textwidth=80
+    au BufRead,BufNewFile *.clj setlocal textwidth=80
 ]]
