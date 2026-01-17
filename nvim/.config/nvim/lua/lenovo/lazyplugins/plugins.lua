3b38ca1f25356abdb80512e9010fbc2af2e4f361
Aitor Calvo Fernández <aitor.calvof@ehu.eus>
2026-01-18 00:36:47 +0100

    add conjure

--- nvim/.config/nvim/lua/lenovo/lazyplugins/plugins.lua
+++ nvim/.config/nvim/lua/lenovo/lazyplugins/plugins.lua
@@ -215,4 +215,38 @@ return {
         "GCBallesteros/jupytext.nvim",
         config = true,
     },
+
+    -- Conjure
+    {
+        "Olical/conjure",
+        ft = { "clojure", "fennel", "python" }, -- etc
+        lazy = true,
+        init = function()
+            -- Set configuration options here
+            -- Uncomment this to get verbose logging to help diagnose internal Conjure issues
+            -- This is VERY helpful when reporting an issue with the project
+            -- vim.g["conjure#debug"] = true
+            vim.g["conjure#mapping#doc_word"] = "gk" -- avoid conflict with LSP
+            -- vim.g["conjure#mapping#doc_word"] = {"K"} -- Reset it to the default unprefixed K (note the special table wrapped syntax)
+
+
+        end,
+
+        -- Optional cmp-conjure integration
+        dependencies = { "PaterJason/cmp-conjure" },
+    },
+    {
+        "PaterJason/cmp-conjure",
+        lazy = true,
+        config = function()
+            local cmp = require("cmp")
+            local config = cmp.get_config()
+            table.insert(config.sources, { name = "conjure" })
+            return cmp.setup(config)
+        end,
+    },
+    { 'tpope/vim-dispatch' },
+    { 'clojure-vim/vim-jack-in' },
+    { 'radenling/vim-dispatch-neovim' },
+
 }
