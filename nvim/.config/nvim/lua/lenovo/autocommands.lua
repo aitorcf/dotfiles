8f95b816e087231070b5bb9c96faec609e2e8087
Aitor Calvo Fernández <aitor.calvof@ehu.eus>
2026-01-27 11:18:24 +0100

    set typst and clojure linewidth

--- nvim/.config/nvim/lua/lenovo/autocommands.lua
+++ nvim/.config/nvim/lua/lenovo/autocommands.lua
@@ -4,5 +4,7 @@ vim.cmd[[
     au BufRead,BufNewFile *.md setlocal textwidth=60
     au BufRead,BufNewFile *.jl setlocal textwidth=80
     au BufRead,BufNewFile *.py setlocal textwidth=80
+    au BufRead,BufNewFile *.typ setlocal textwidth=80
     au BufRead,BufNewFile *.clj setlocal textwidth=80
+    au BufRead,BufNewFile *.clj setlocal filetype=clojure
 ]]
