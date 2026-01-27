846165c412e8022cfba9e4af2f1c1eae02a2df1a
Aitor Calvo Fernández <aitor.calvof@ehu.eus>
2026-01-27 11:18:51 +0100

    add tinymist config

--- nvim/.config/nvim/lua/lenovo/lsp.lua
+++ nvim/.config/nvim/lua/lenovo/lsp.lua
@@ -7,3 +7,12 @@ vim.lsp.config("jetls", {
     filetypes = {"julia"},
 })
 vim.lsp.enable("jetls")
+
+-- vim.lsp.config("tinymist", {
+--     settings = {
+--         formatterMode = "typstyle",
+--         exportPdf = "onType",
+--         semanticTokens = "disable"
+--     }
+-- })
+
